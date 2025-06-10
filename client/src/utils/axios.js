d98e90e5c4102720a4a17a37384a8bae635f0576
jupiterbania009 <www.jupiterbania009@gmail.com>
2025-06-10 23:40:28 +0530

    Add files via upload

---
+++ client/src/utils/axios.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+// Add a request interceptor
+instance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            // Clear token and redirect to login if unauthorized
+            localStorage.removeItem('token');
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default instance; 

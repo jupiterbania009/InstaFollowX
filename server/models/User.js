37162c3e76be6b553e8df9e6bd6a98a869b9be56
jupiterbania009 <www.jupiterbania009@gmail.com>
2025-06-11 10:17:21 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -25,7 +25,33 @@ const userSchema = new mongoose.Schema({
     },
     points: {
         type: Number,
-        default: 5  // Start with 5 points
+        default: 5  // Start with 5 bonus points
+    },
+    followersCount: {
+        type: Number,
+        default: 0
+    },
+    followingCount: {
+        type: Number,
+        default: 0
+    },
+    rating: {
+        average: {
+            type: Number,
+            default: 5.0
+        },
+        count: {
+            type: Number,
+            default: 0
+        }
+    },
+    followBackRate: {
+        type: Number,
+        default: 100  // Percentage
+    },
+    lastActive: {
+        type: Date,
+        default: Date.now
     },
     isVerified: {
         type: Boolean,
@@ -34,6 +60,10 @@ const userSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    rank: {
+        type: Number,
+        default: 0
     }
 });
 
@@ -60,7 +90,8 @@ userSchema.methods.matchPassword = async function(enteredPassword) {
 // Method to add points
 userSchema.methods.addPoints = async function(amount) {
     this.points += amount;
-    return this.save();
+    await this.save();
+    return this.points;
 };
 
 // Method to deduct points
@@ -69,12 +100,47 @@ userSchema.methods.deductPoints = async function(amount) {
         throw new Error('Insufficient points');
     }
     this.points -= amount;
-    return this.save();
+    await this.save();
+    return this.points;
 };
 
 // Method to check if user has enough points
 userSchema.methods.hasEnoughPoints = function(amount) {
     return this.points >= amount;
 };
+
+// Method to update follow counts
+userSchema.methods.updateFollowCounts = async function(followingDelta, followersDelta) {
+    this.followingCount += followingDelta;
+    this.followersCount += followersDelta;
+    await this.save();
+};
+
+// Method to update rating
+userSchema.methods.updateRating = async function(newRating) {
+    const oldTotal = this.rating.average * this.rating.count;
+    this.rating.count += 1;
+    this.rating.average = (oldTotal + newRating) / this.rating.count;
+    await this.save();
+};
+
+// Method to update follow back rate
+userSchema.methods.updateFollowBackRate = async function(didFollowBack) {
+    const weight = 0.7; // Weight for new data vs historical
+    this.followBackRate = (this.followBackRate * (1 - weight)) + (didFollowBack ? 100 : 0) * weight;
+    await this.save();
+};
+
+// Method to update last active timestamp
+userSchema.methods.touch = async function() {
+    this.lastActive = new Date();
+    await this.save();
+};
+
+// Indexes for efficient querying
+userSchema.index({ points: -1 }); // For leaderboard
+userSchema.index({ rating: -1 }); // For user discovery
+userSchema.index({ lastActive: -1 }); // For active users
+userSchema.index({ followBackRate: -1 }); // For reliable users
 
 module.exports = mongoose.model('User', userSchema); 
